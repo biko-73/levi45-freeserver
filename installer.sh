f69e75c7febf85b1ff83fec8859e2954d626add4
biko-73 <57875905+biko-73@users.noreply.github.com>
2025-12-31 16:44:20 +0300

    Update package versions in installer script

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='levi45-freeserver/main'
 
-MY_IPK="enigma2-plugin-extensions-levi45-freeserver_2.0_all.ipk"
-MY_DEB="enigma2-plugin-extensions-levi45-freeserver_2.0_all.deb"
+MY_IPK="enigma2-plugin-extensions-levi45-freeserver_2.2_all.ipk"
+MY_DEB="enigma2-plugin-extensions-levi45-freeserver_2.2_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
