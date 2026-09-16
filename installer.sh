b379209d6d553878fbffbd9961a3d6f220467687
biko-73 <57875905+biko-73@users.noreply.github.com>
2026-09-16 19:10:42 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,7 +12,7 @@
 
 PACKAGE_DIR='levi45-freeserver/main'
 
-MY_IPK="enigma2-plugin-extensions-levi45-freeserver_2.4_all.ipk"
+MY_IPK="enigma2-plugin-extensions-levi45-freeserver_2.6_all.ipk"
 MY_DEB="enigma2-plugin-extensions-levi45-freeserver_2.5_all.deb"
 
 ########################################################################################################################
